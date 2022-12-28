31925607312e07da6729d2f1b17a8a1ff9043ad0
babakovn <babakovn@gmail.com>
2022-12-28 16:18:46 +0300

    Final

--- Program.cs
+++ Program.cs
@@ -1,2 +1,41 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string[] Getstringarray(int len)
+{
+    string[] strarr = new string[len];
+    for(int i = 0; i < len; i++)
+    {
+        Console.Write("Введите строку массива:");
+        strarr[i] = Console.ReadLine();
+
+    }
+    return strarr;
+}
+
+void Printstringarr(string[] arr)
+{
+    for(int i = 0; i < arr.GetLength(0); i++)
+    {
+        Console.Write($"{arr[i]} ");
+    }
+}
+
+//
+
+int len = 0;
+Console.Write("Введите количество строк в масииве:");
+int.TryParse(Console.ReadLine(), out len);
+
+string[] arr = Getstringarray(len);
+
+string[] arrexit = new string[len];
+
+int j = 0;
+for(int i = 0; i < arr.GetLength(0); i++)
+{
+    if(arr[i].Length < 4)
+    {
+        arrexit[j] = arr[i];
+        j++;
+    }
+}
+
+Printstringarr(arrexit);
